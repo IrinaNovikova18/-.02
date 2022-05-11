ef45c38a1887110e03a5677860b248798f4127a6
18ipp18 <18ipp18@stud.ypec.ru>
2022-05-11 09:00:34 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Class1
+    {
+        private static Entities _context;
+        public static Entities GetEntities()
+        {
+            if (_context == null)
+                _context = new Entities();
+            return _context;
+        }
+    }
+}
